6ede6b9d46ddaad450190693e31baaa2c6f51c3c
wcelery <horoschun.yura@yandex.ru>
2021-05-19 12:43:37 +0800

    additional fileds added

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,34 +6,64 @@ export default function Modal() {
   const [selected, setSelected] = React.useState();
   console.log(selected);
 
+  const optionValues = ["None", "Holidays", "Events", "Other"];
+
   const additionalFields = (selected) => {
     switch (selected) {
-      case "Пункт 1":
-        return <h1>test</h1>;
+      case "None":
+        return null;
 
-      case "Пункт 2":
-        return <input>36</input>;
+      case "Holidays":
+        return (
+          <>
+            <label htmlFor="toSpend">Budget</label>
+            <input type="text" name="toSpend" id="toSpend"></input>
+          </>
+        );
+
+      case "Events":
+        return (
+          <>
+            <label htmlFor="where">Where?</label>
+            <input type="text" name="where" id="where"></input>
+            <label htmlFor="when">When?</label>
+            <input type="text" name="when" id="when"></input>
+          </>
+        );
+
+      case "Other":
+        return (
+          <>
+            <label htmlFor="note">Notes</label>
+            <input type="text" name="note" id="note"></input>
+          </>
+        );
 
       default:
         return null;
     }
   };
+
   return (
     <div>
       <h1>test</h1>
-      <button onClick={() => setIsOpen(!isOpen)}>test</button>
-      <select
-        onChange={(e) => {
-          setSelected(() => e.target.value);
-        }}
-      >
-        <option>Пункт 1</option>
-        <option>Пункт 2</option>
-      </select>
-      <input type="text" name="Name" />
-      <input type="where" name="Name" />
-      <input type="when" name="Name" />
-      {additionalFields()}
+      <button onClick={() => setIsOpen(!isOpen)}>Edit</button>
+      <button onClick={() => setIsOpen(!isOpen)}>Delete</button>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input type="text" name="what" />
+        <select
+          value={selected}
+          onChange={(e) => {
+            setSelected(e.target.value);
+          }}
+        >
+          {optionValues.map((option) => (
+            <option value={option}>{option}</option>
+          ))}
+        </select>
+        {additionalFields(selected)}
+        <button type="submit">Create</button>
+      </form>
     </div>
   );
 }
