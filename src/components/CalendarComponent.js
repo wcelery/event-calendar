3d968d7a75abcad841523c95cdd0a903d437ba23
wcelery <horoschun.yura@yandex.ru>
2021-05-25 14:29:01 +0800

    highlight dates with events

--- src/components/CalendarComponent.js
+++ src/components/CalendarComponent.js
@@ -4,15 +4,18 @@ import "react-calendar/dist/Calendar.css";
 import { CalendarContext } from "../context/CalendarContext";
 
 export default function CalendarComponent() {
-  const { isOpen, setIsOpen, dispatch } = React.useContext(CalendarContext);
+  const { isOpen, setIsOpen, events, dispatch } =
+    React.useContext(CalendarContext);
 
   const highlightedDates = [
     new Date(2021, 4, 25, 0, 0, 0).toISOString(),
     new Date(2021, 4, 26, 0, 0, 0).toISOString(),
   ];
 
+  const extractedDatesFrom = Object.values(events).map((value) => value.date);
+
   const getClassName = (date) => {
-    if (highlightedDates.includes(date.toISOString())) {
+    if (extractedDatesFrom.includes(date.toISOString())) {
       return "highlighted_dates";
     } else {
       return "not_highlighted_dates";
--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -4,20 +4,16 @@ import { CalendarContext } from "../context/CalendarContext";
 export default function EventsList() {
   const { events, date } = React.useContext(CalendarContext);
 
-  const newObj = Object.values(events).filter((value) => value.date === date);
-  /* .reduce((obj, key) => {
-    obj[key] = buildingEvent[key];
-    return obj;
-  }, {}); */
+  const selectedDateEvents = Object.values(events).filter(
+    (value) => value.date === date
+  );
 
-  console.log(newObj);
   return (
     <section className="events-list">
       <h1>tse</h1>
-      {Object.values(newObj).map((val1) => (
+      {Object.values(selectedDateEvents).map((val1) => (
         <>
           <h1>{val1.name}</h1>
-          {/*           <h1>{val1.Budget}</h1> */}
         </>
       ))}
     </section>
